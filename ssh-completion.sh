82b78a3f3010abdf26e8f29219f4bcee151b286a
Cleveland Flowe <flowe.c@gmail.com>
2013-09-06 15:20:50 -0400

    Initial version

---
+++ ssh-completion.sh
@@ -0,0 +1,23 @@
+#!/bin/bash # for correct bash/vim highlighting
+
+if [ -n "${BASH_VERSION:-}" ]; then
+  # This dynamically determines the hosts
+  _ssh_completion()
+  {
+      local cur names
+      cur="${COMP_WORDS[COMP_CWORD]}"
+      COMPREPLY=()
+
+      if [ ${#COMP_WORDS[@]:-0} -lt 3 ]; then
+        names=$(awk '/^[ \t]*Host[ \t]+/ {print $2}' ~/.ssh/config  | sort | uniq)
+        COMPREPLY=( $(compgen -W "${names}" -- ${cur}) )
+      fi
+
+      return 0
+  }
+
+  complete -F _ssh_completion ssh
+
+  # this statically determines the hosts
+  #complete -W "$(awk '/^\s*Host\s*/ {print $2}' ~/.ssh/config  | sort | uniq)" ssh
+fi
