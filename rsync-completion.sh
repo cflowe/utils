8b0d133dc2604ce66bb7e0ba11b3cf3f2ea35bad
Cleveland Flowe <flowe.c@gmail.com>
2013-11-25 21:46:51 -0500

    Added support for multiple entries on the Host line

--- rsync-completion.sh
+++ rsync-completion.sh
@@ -2,7 +2,7 @@
 
 if [ -n "${BASH_VERSION:-}" ]; then
   # This dynamically determines the hosts
-  _ssh_completion()
+  _rsync_completion()
   {
       local cur names
       cur="${COMP_WORDS[COMP_CWORD]}"
@@ -15,8 +15,8 @@ if [ -n "${BASH_VERSION:-}" ]; then
       return 0
   }
 
-  complete -o filenames -F _ssh_completion ssh
+  complete -o filenames -F _rsync_completion rsync
 
   # this statically determines the hosts
-  #complete -W "$(egrep '^[[:space:]]*Host[[:space:]]+' ~/.ssh/config  | sed -re 's/^[[:space:]]*Host[[:space:]]*//g; s/[[:space:]]+/\n/g' | sort | uniq)" ssh
+  #complete -W "$(egrep '^[[:space:]]*Host[[:space:]]+' ~/.ssh/config  | sed -re 's/^[[:space:]]*Host[[:space:]]*//g; s/[[:space:]]+/\n/g' | sort | uniq)" rsync
 fi
