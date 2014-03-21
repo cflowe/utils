9e6e790cf53640f9a86af9bca452ad1d10f6d713
Cleveland Flowe <flowe.c@gmail.com>
2014-03-21 00:31:45 -0400

    Handle the case where ~/.ssh/config does not exist

--- ssh-completion.sh
+++ ssh-completion.sh
@@ -8,9 +8,11 @@ if [ -n "${BASH_VERSION:-}" ]; then
       cur="${COMP_WORDS[COMP_CWORD]}"
       COMPREPLY=()
 
-      names=$(egrep '^[[:space:]]*Host[[:space:]]+' ~/.ssh/config  | sed -re 's/^[[:space:]]*Host[[:space:]]*//g; s/[[:space:]]+/\n/g' | sort | uniq)
+      if [ -e ~/.ssh/config ]; then
+        names=$(egrep '^[[:space:]]*Host[[:space:]]+' ~/.ssh/config  | sed -re 's/^[[:space:]]*Host[[:space:]]*//g; s/[[:space:]]+/\n/g' | sort | uniq)
 
-      COMPREPLY=( $(compgen -f -W "${names}" -- ${cur}) )
+        COMPREPLY=( $(compgen -f -W "${names}" -- ${cur}) )
+      fi
 
       return 0
   }
