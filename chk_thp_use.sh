f4aa5acfee6632c57cf08f1c0b127001af69c982
Cleveland Flowe <cflowe@airsage.com>
2014-11-05 17:40:08 -0500

    Show the the ammount of thp used per process in kB

---
+++ chk_thp_use.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# This script shows the the amount of thp used per process in kB.
+
+pattern=${1:-.*}
+
+for pid in $(pgrep -f -- "$pattern")
+do
+  prog=$(readlink -f /proc/$pid/exe)
+
+  if [ -z "$prog" ]; then
+    # pids without an exe are usually kernel threads
+    continue
+  fi
+
+  echo "$prog: $pid: $(awk '/^AnonHugePages:/{i+=$2} END {print i}' /proc/$pid/smaps)"
+done
